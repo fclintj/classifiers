02710bcfca13856a399a71907541eddf5dde458d
Clint Ferrin <clint.ferrin@gmail.com>
2017-09-25 13:55:40 -0600

    working bayes

--- python/15_naive_bayes_applied.py
+++ python/15_naive_bayes_applied.py
@@ -5,9 +5,9 @@ import matplotlib.ticker as ticker
 import math
 
 class data_frame:
-    def __init__(self, data):
-        self.x0 = data[:,0:2].T
-        self.x1 = data[:,2:4].T
+    def __init__(self, data0, data1):
+        self.x0 = data0 
+        self.x1 = data1 
         self.xtot = np.c_[self.x0,self.x1]
         self.N0 = self.x0.shape[1]
         self.N1 = self.x1.shape[1]
@@ -37,8 +37,11 @@ def gendata2(class_type,N):
         x = np.c_[x, [[m[0]],[m[1]]] + np.random.randn(2,1)/np.sqrt(5)]
     return x 
 
-def getParzen(data,pts,lam):
-    # give it x/y pairs and it returns linspaces and Parzen pdf
+def gen_test_df(num0,num1):
+
+    return data_frame
+
+def get_parzen(data,pts,lam):
     x = np.linspace(min(data)-3*lam,max(data)+3*lam, pts)
     kernel = np.empty([data.size,pts])
     for i in range(data.size):
@@ -51,37 +54,81 @@ def getParzen(data,pts,lam):
    
     return x, parzen
 
+def class_parzen(data0,pts,lam):
+    x0,parzen_x0 = get_parzen(data0[0,:],pts,lam)
+    y0,parzen_y0= get_parzen(data0[1,:],pts,lam)
+    return [x0,y0],[parzen_x0,parzen_y0]
+
+def prob2d(point,linspace0,parzen0):
+    prob_x = np.interp(point[0],linspace0[0],parzen0[0])
+    prob_y = np.interp(point[1],linspace0[1],parzen0[1])
+    # print("prob x: %f, prob y: %f"%(prob_x, prob_y))
+    return prob_x*prob_y     
+
+def run_bayes_test(data_tot,linspace,parzen):
+    y = np.r_[np.zeros([data_tot.shape[1],1]),np.ones([data_tot.shape[1],1])] 
+    y_hat = np.zeros([data_tot.shape[1],1])
+
+    for i in range(data_tot.shape[1]):
+        prob0 = prob2d(data_tot[:,i],linspace[0],parzen[0])
+        prob1 = prob2d(data_tot[:,i],linspace[1],parzen[0])
+        if prob1 > prob0:
+            y_hat[i] = 1 
+
+    return y_hat
+
+
 data = np.loadtxt("../data/classasgntrain1.dat",dtype=float)
-data = data_frame(data)
+x0 = data[:,0:2].T
+x1 = data[:,2:4].T
+data = data_frame(x0,x1)
 
 pts = 100
 lam = 0.8
 
-x0,parzen_x0 = getParzen(data.x0[0,:],pts,lam)
-y0,parzen_y0= getParzen(data.x0[1,:],pts,lam)
+linspace0,parzen0 = class_parzen(data.x0,pts,lam)
+linspace1,parzen1 = class_parzen(data.x1,pts,lam)
 
-x1,parzen_x1 = getParzen(data.x1[0,:],pts,lam)
-y1,parzen_y1= getParzen(data.x1[1,:],pts,lam)
+linspace = np.array([linspace0,linspace1])
+parzen = np.array([parzen0,parzen1])
 
-plt.plot(x1,parzen_x1)
-plt.plot(y1,parzen_y1)
+y = np.r_[np.zeros([data.N1,1]),np.ones([data.N0,1])] 
+y_hat = run_bayes_test(data.xtot,linspace,parzen)
 
-prob = np.interp(-10,x,parzen)
-# print(prob)
+num_err = sum(abs(y_hat - y))
+print("Percent of errors: %.2f"%(float(num_err)/data.N))
+
+
+xtest0 = gendata2(0,10000)
+xtest1 = gendata2(1,10000) 
+test_data = data_frame(xtest0,xtest1)
+y = np.r_[np.zeros([test_data.N1,1]),np.ones([test_data.N0,1])] 
+
+y_hat = run_bayes_test(test_data.xtot,linspace,parzen)
+
+num_err = sum(abs(y_hat - y))
+print("Percent of errors: %.2f"%(float(num_err)/data.N))
+
+xp1 = np.linspace(data.xlim[0],data.xlim[1], num=100)
+yp1 = np.linspace(data.ylim[0],data.ylim[1], num=100) 
+
+
+
+red_pts = np.array([[],[]])
+green_pts= np.array([[],[]])
+
+
+for x in xp1:
+    for y in yp1:
+        prob0 = prob2d([x,y],linspace[0],parzen[0])
+        prob1 = prob2d([x,y],linspace[1],parzen[0])
+        if prob1 > prob0: 
+            green_pts = np.c_[green_pts,[x,y]]
+        else:
+            red_pts = np.c_[red_pts,[x,y]]
+
+plt.scatter(green_pts[0,:],green_pts[1,:],color='blue',s=0.25)
+plt.scatter(red_pts[0,:],red_pts[1,:],color='red',s=0.25)
+plt.xlim(data.xlim)
+plt.ylim(data.ylim)
 plt.show()
-
-# emperical = np.interp(np.random.rand(pts,1),cdf,x)
-# emperical = np.sort(emperical,axis=None)
-#
-# ax[1].stem(emperical,np.ones(pts),'b',markerfmt=' ')
-# ax[1].set_xlim(0,14)
-#
-#
-# parzen = np.empty(100)
-# for i in range(pts):
-#     parzen[i] = sum(kernel[:,i])/pts
-#
-# ax[3].plot(x,parzen)
-#
-# plt.show()
-#
